037daac271cd97866ccaf3eff29110a64f0ad0f1
cnrd <hello@conradweise.com>
2019-05-21 12:10:02 +0200

    ip

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const WS_URL = window.location.hostname === 'localhost' ? 'ws://localhost:3001' : 'ws://192.168.178.101:3001'
+const WS_URL = window.location.hostname === 'localhost' ? 'ws://localhost:3001' : 'ws://192.168.0.101:3001'
 const socket = new WebSocket(WS_URL)
 
 function passed (token) {
